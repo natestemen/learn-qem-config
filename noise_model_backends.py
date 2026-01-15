d7dea0d27b6f5a619c2444974658c421007faecb
Sebastian Goossens <basgooss@gmail.com>
2026-01-15 12:31:58 +0100

    added thermal noise relaxation model

--- noise_model_backends.py
+++ noise_model_backends.py
@@ -1,5 +1,5 @@
 from qiskit_aer import AerSimulator
-from qiskit_aer.noise import NoiseModel, depolarizing_error, phase_damping_error, amplitude_damping_error, ReadoutError
+from qiskit_aer.noise import NoiseModel, depolarizing_error, phase_damping_error, amplitude_damping_error, ReadoutError, thermal_relaxation_error
 
 def _build_depolarizing_backend(prob=0.005, **args):
     noise_model = NoiseModel()
@@ -32,6 +32,17 @@ def _build_readout_backend(prob=0.005, **args):
 
     readout_err = ReadoutError([[1 - prob, prob], [prob, 1 - prob]])
     noise_model.add_all_qubit_readout_error(readout_err)
+
+    return AerSimulator(noise_model=noise_model)
+
+def _build_thermal_relaxation_backend(T1=50e3, T2=30e3, gate_time_1q=50, gate_time_2q=150, **args):
+    noise_model = NoiseModel()
+    
+    thermal_err_1q = thermal_relaxation_error(T1, T2, gate_time_1q)
+    thermal_err_2q = thermal_relaxation_error(T1, T2, gate_time_2q).tensor(thermal_relaxation_error(T1, T2, gate_time_2q))
+    
+    noise_model.add_all_qubit_quantum_error(thermal_err_1q, ["h", "x", "y", "z"])
+    noise_model.add_all_qubit_quantum_error(thermal_err_2q, ["cx"])
 
     return AerSimulator(noise_model=noise_model)
 
