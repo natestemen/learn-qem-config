0261e3fccad1357517c51994c3c66525b71b0492
Sebastian Goossens <basgooss@gmail.com>
2026-01-20 17:28:42 +0100

    Added a general noise backend suited for ZNE parameter optimization

--- noise_model_backends.py
+++ noise_model_backends.py
@@ -35,7 +35,7 @@ def _build_readout_backend(prob=0.005, **args):
 
     return AerSimulator(noise_model=noise_model)
 
-def _build_thermal_relaxation_backend(T1=50e3, T2=30e3, gate_time_1q=50, gate_time_2q=150, **args):
+def _build_thermal_relaxation_backend(T1=50e3, T2=70e3, gate_time_1q=50, gate_time_2q=150, **args):
     noise_model = NoiseModel()
 
     thermal_err_1q = thermal_relaxation_error(T1, T2, gate_time_1q)
@@ -46,13 +46,38 @@ def _build_thermal_relaxation_backend(T1=50e3, T2=30e3, gate_time_1q=50, gate_ti
 
     return AerSimulator(noise_model=noise_model)
 
+#This backend combines thermal and depolarizing noise for ZNE experiments
+# since these are best suited for general ZNE optimization. 
+
+
+def _build_general_zne_backend(prob_1q=0.002, prob_2q=0.008, T1=50e3, T2=70e3, gate_time_1q=50, gate_time_2q=300)
+    noise_model = NoiseModel()
+    #single qubit
+    depol_1q = depolarizing_error(prob_1q, 1)
+    thermal_1q = thermal_relaxation_error(T1, T2, gate_time_1q)
+    err_1q = depol_1q.compose(thermal_1q)
+
+    noise_model.add_all_qubit_quantum_error(err_1q, ["h", "x", "y", "z"])
+
+    #two qubit
+    depol_2q = depolarizing_error(prob_2q, 2)
+    thermal_2q = thermal_relaxation_error(T1, T2, gate_time_2q).tensor(thermal_relaxation_error(T1, T2, gate_time_2q))
+    err_2q = depol_2q.compose(thermal_2q)
+    noise_model.add_all_qubit_quantum_error(err_2q, ["cx"])
+
+    return AerSimulator(noise_model=noise_model)
+
+
+
+
 
 NOISE_BACKEND_MAP = {
     "depolarizing": _build_depolarizing_backend,
     "amplitude_damping": _build_amplitude_damping_backend,
     "phase_damping": _build_phase_damping_backend,
     "readout": _build_readout_backend,
-    "thermal": _build_thermal_relaxation_backend
+    "thermal": _build_thermal_relaxation_backend,
+    "general_zne": _build_general_zne_backend
 }
 
 def get_noise_backend(noise_model_name, **args):
