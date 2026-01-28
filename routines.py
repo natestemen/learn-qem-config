d42d3ffee0bba8e764a4211f099cff51391eafe8
JorgeEzpeleta <jorge.ezpeleta.vr@gmail.com>
2026-01-28 19:15:29 +0100

    Add adaptative strategy, improve logic, add experiment.py to separate creation of the experiment logic

--- routines.py
+++ routines.py
@@ -1,81 +1,203 @@
 import json
 import numpy as np
-import copy
-from test_zne import make_experiment_list, batch_execute, make_executor
-from noise_model_backends import get_noise_backend
-from circuits import get_experiment
+import re
+from experiment import make_experiment_list, batch_execute
 
-NOISE_MODEL = "depolarizing"
-CIRCUIT = "mirror_circuits"
+DEFAULT_METHODS = ["global", "local_random", "local_all", "identity_scaling"]
+DEFAULT_EXTRAPS = ["linear", "richardson", "polynomial", "exponential", "poly-exp", "adaptive-exp"]
+DEFAULT_SEARCH_FACTORS = [[1, 2, 3], [1, 3, 5], [1, 5, 9]]
+BASE_FACTORS = [1, 3, 5, 7]
 
-search_space = {
-    "noise_scaling_factors": [[1, 1.25, 1.5],[1, 2, 3],[2, 4, 6],],
-    "noise_scaling_method": ["global", "local_all", "local_random"],
-    "extrapolation": ["polynomial", "linear"],
-}
+def _print_result(best_config, ideal_result, best_result, error):
 
-param_order = ["noise_scaling_factors", "noise_scaling_method", "extrapolation"]
-
-circ, verify_func, ideal_result = get_experiment(CIRCUIT)
-backend = get_noise_backend(NOISE_MODEL)
-exe = make_executor(backend, verify_func, shots=4096)
-
-# Starting configuration: first value of each parameter
-current_config = {
-    "noise_scaling_factors": search_space["noise_scaling_factors"][0],
-    "noise_scaling_method": search_space["noise_scaling_method"][0],
-    "extrapolation": search_space["extrapolation"][0]
-}
-
-iter = 0
-max_iter=10
-converged = False
-max_reached=False
-
-while not converged and not max_reached:
-    iter += 1
+    print("BEST CONFIGURATION:")
     
-    previous_config = current_config.copy()
+    # Json manipulation for better print
+    json_str = json.dumps(best_config, indent=4)
+    json_str = re.sub(r'\[\s+([^]]+?)\s+\]', lambda x: "[" + " ".join(x.group(1).split()) + "]", json_str)
     
-    # Optimize locally each parameter
-    for param_name in ["noise_scaling_factors", "noise_scaling_method", "extrapolation"]:
+    print(json_str)
+    print(f"Ideal result:      {ideal_result}")
+    print(f"Best result:       {best_result}")
+    print(f"Absolute error:    {error}")
+
+def _get_default_batch():
+    return {
+        "noise_scaling_factors": DEFAULT_SEARCH_FACTORS,
+        "noise_scaling_method": DEFAULT_METHODS,
+        "extrapolation": DEFAULT_EXTRAPS
+    }
+
+
+def brute_force_search(circ, exe, ideal_result, zne_batch_test=None):
+
+    # Set the default search space if not provided
+    if zne_batch_test is None:
+        zne_batch_test = _get_default_batch()
         
-        # Set up an experiment with all parameters fixed except one
-        batch_dict = {
-            "noise_scaling_factors": [current_config["noise_scaling_factors"]],
-            "noise_scaling_method": [current_config["noise_scaling_method"]],
-            "extrapolation": [current_config["extrapolation"]]
+    exp_results = batch_execute(zne_batch_test, circ, exe)
+
+    # Error handling
+    results_array = np.array(exp_results, dtype=float)
+    num_failures = np.isnan(results_array).sum()
+    if num_failures > 0:
+        print(f"{num_failures} configurations failed due to extrapolation errors")
+
+    configs = make_experiment_list(zne_batch_test)
+    errors = np.abs(np.array(exp_results) - ideal_result)
+    best_idx = np.nanargmin(errors)
+
+    _print_result(best_config=configs[best_idx], ideal_result=ideal_result, best_result=exp_results[best_idx], error=errors[best_idx])
+
+
+def light_brute_force_search(circ, exe, ideal_result, zne_batch_test=None, max_iter=10, printing=True):
+
+    # Set the default search space if not provided
+    if zne_batch_test is None:
+        zne_batch_test = _get_default_batch()
+
+    # Starting configuration: first value of each parameter
+    current_config = {
+        "noise_scaling_factors": zne_batch_test["noise_scaling_factors"][0],
+        "noise_scaling_method": zne_batch_test["noise_scaling_method"][0],
+        "extrapolation": zne_batch_test["extrapolation"][0]
+    }
+
+    iter = 0
+    converged = False
+    max_reached = False
+    
+    best_result_value = np.nan
+
+    while not converged and not max_reached:
+        iter += 1
+        previous_config = current_config.copy()
+        
+        # Optimize locally each parameter
+        for param_name in ["noise_scaling_factors", "noise_scaling_method", "extrapolation"]:
+            
+            # Skip if there is only 1 option for this parameter 
+            if len(zne_batch_test[param_name]) <= 1:
+                continue
+
+            # Set up an experiment with all parameters fixed except one
+            batch_dict = {
+                "noise_scaling_factors": [current_config["noise_scaling_factors"]],
+                "noise_scaling_method": [current_config["noise_scaling_method"]],
+                "extrapolation": [current_config["extrapolation"]]
+            }
+            batch_dict[param_name] = zne_batch_test[param_name]
+
+            # Run this batch of experiments
+            results = batch_execute(batch_dict, circ, exe)
+            
+            # Error handling
+            # Convert to numpy array to handle NaNs correctly
+            results_array = np.array(results, dtype=float)
+            errors = np.abs(results_array - ideal_result)
+            # If all experiments in this batch failed (all are NaN), skip update
+            if np.all(np.isnan(errors)):
+                continue
+
+            best_idx = np.nanargmin(errors)
+            
+            best_value_for_param = zne_batch_test[param_name][best_idx]
+            current_config[param_name] = best_value_for_param
+            best_result_value = results[best_idx]
+
+        # Check if the configuration has changed
+        if json.dumps(previous_config, sort_keys=True) == json.dumps(current_config, sort_keys=True):
+            converged = True
+
+        # Limit on iterations
+        if iter == max_iter:
+            max_reached = True
+
+    final_error = np.nan
+    if not np.isnan(best_result_value):
+        final_error = abs(best_result_value - ideal_result)
+
+    # Only print results when it is used as an independet routine
+    if printing:
+        if max_reached:
+            print("Max number of iterations reached")
+        if np.isnan(best_result_value):
+            print("\nLight Brute Force failed: No valid configuration found.")
+        else:
+            _print_result(best_config=current_config, ideal_result=ideal_result, best_result=best_result_value, error=final_error)
+            
+    return current_config, best_result_value, final_error
+
+
+def adaptive_search(circ, exe, ideal_result, zne_batch_test=None, tolerance=1e-3, max_factors=10):
+
+    stop_reason=0
+
+    # Setup searching space, use user methods but force the default scaling factors    
+    if zne_batch_test is None:
+        methods_to_test = DEFAULT_METHODS
+        extraps_to_test = DEFAULT_EXTRAPS
+    else:
+        methods_to_test = zne_batch_test.get("noise_scaling_method", DEFAULT_METHODS)
+        extraps_to_test = zne_batch_test.get("extrapolation", DEFAULT_EXTRAPS)
+
+    # Force the base factors for the initial search
+    initial_batch = {
+        "noise_scaling_factors": [BASE_FACTORS],
+        "noise_scaling_method": methods_to_test,
+        "extrapolation": extraps_to_test
+    }
+
+    # Find best methods using light_brute_force 
+    best_config, best_val, best_err = light_brute_force_search( circ, exe, ideal_result, initial_batch, max_iter=10, printing=False)
+
+    # Adaptive expansion of scale factors
+
+    current_factors = list(best_config["noise_scaling_factors"])
+    current_best_error = best_err
+    current_best_result = best_val
+    
+    next_factor = 9
+    
+    while len(current_factors) < max_factors:
+        
+        # Add one scale factor
+        new_factors_list = current_factors + [next_factor]
+        
+        # Build single experiment for validation
+        test_batch = {
+            "noise_scaling_factors": [new_factors_list],
+            "noise_scaling_method": [best_config["noise_scaling_method"]],
+            "extrapolation": [best_config["extrapolation"]]
         }
 
-        batch_dict[param_name] = search_space[param_name]
+        # Execute
+        results = batch_execute(test_batch, circ, exe)
+        new_val = results[0]
         
-        # Run this batch of experiments and find the best configuration
-        results = batch_execute(batch_dict, circ, exe)
-        errors = np.abs(np.array(results) - ideal_result)
-        best_idx = np.argmin(errors)
-        best_value_for_param = search_space[param_name][best_idx]
-        best_result_value = results[best_idx]
-
-        # Update current configuration
-        current_config[param_name] = best_value_for_param
+        # Check if there is any reasonable improvement
+        new_err = abs(new_val - ideal_result)
+        improvement = current_best_error - new_err
+        if new_err > current_best_error:
+            stop_reason=1
+            break 
+        if improvement < tolerance:
+            stop_reason=2
+            break
         
+        # Update scale factor list
+        current_factors = new_factors_list
+        current_best_error = new_err
+        current_best_result = new_val
+        best_config["noise_scaling_factors"] = current_factors
+        next_factor += 2
 
-    # Check if the configuration has changed
-    start_str = json.dumps(previous_config, sort_keys=True)
-    end_str = json.dumps(current_config, sort_keys=True)
-    if start_str == end_str:
-        converged = True
-    # If it has change, restart the loop
-    # Limit on iterations
-    if iter==max_iter:
-        max_reached=True
+    # Print the stopping criteria
+    if stop_reason==1:
+        print("Adaptive search stopped: error worsened")
+    elif stop_reason==2:
+        print("Adaptive search: Convergence reached")
+    elif stop_reason==0:
+        print("Adaptive search: Maximum limit of scale factors reached ")
 
-if max_reached:
-    print("Max number of iterations reached")
-else:
-    final_error = abs(best_result_value - ideal_result)
-    print("BEST CONFIGURATION:")
-    print(json.dumps(current_config, indent=4))
-    print(f"Ideal result:      {ideal_result}")
-    print(f"Best result:   {best_result_value}")
-    print(f"Absolute error:   {final_error}")
+    _print_result(best_config, ideal_result, current_best_result, current_best_error)
--- test_zne.py
+++ test_zne.py
@@ -1,175 +1,25 @@
-import itertools
-import json
-
-import jsonschema
-import numpy as np
-from mitiq import zne
-from mitiq.zne.scaling.folding import fold_all, fold_gates_at_random, fold_global
-from mitiq.zne.scaling.identity_insertion import insert_id_layers
-from mitiq.zne.scaling.layer_scaling import get_layer_folding
-from qiskit import QuantumCircuit, transpile
+from experiment import make_executor
+import experiment
+from routines import light_brute_force_search, brute_force_search, adaptive_search
 from noise_model_backends import get_noise_backend
 from circuits import get_experiment
 
 NOISE_MODEL="depolarizing"                   #{depolarizing, amplitude_damping, phase_damping, readout, thermal, general_zne}
-CIRCUIT="mirror_circuits"                                  #{ghz, mirror_circuits, rb_circuits, rotated_rb_circuits, random_clifford_t, w_state, qpe}
+CIRCUIT="ghz"                                #{ghz, mirror_circuits, rb_circuits, rotated_rb_circuits, random_clifford_t, w_state, qpe}
 
-
-# Load Schema:
-def load(schema_path):
-    with open(schema_path, "r") as file:
-        return json.load(file)
-
-zne_schema = load("zne.json")
+# Turn on / off warnings (Better to turn it off to work with several experiments)
+experiment.SILENCE_WARNINGS=True
 
 # Create a test "batch" experiment:
 zne_batch_test = {
-    "noise_scaling_factors": [
-        [1, 1.25, 1.5],
-        [1, 2, 3],
-        [2, 4, 6],
-    ],  # Noise scaling values
-    #"noise_scaling_method": ["global"],  # Folding method
-    "noise_scaling_method": ["global", "local_all", "local_random"],
-    "extrapolation": ["polynomial", "linear"],  # Extrapolation method
+    "noise_scaling_factors": [[1, 3, 5, 7], [1, 3, 5]],  # Noise scaling values
+    "noise_scaling_method": ["global", "local_random", "local_all", "identity_scaling"],
+    "extrapolation": ["linear", "richardson", "polynomial", "exponential", "poly-exp", "adaptive-exp"],
 }
-
-# Create single experiments from batch object:
-
-# Define a function to make all combinations of experiments from a "batch dictionary" which should be formatted like the test case above
-def make_experiment_list(batch_dict):
-    # Initialize empty list where we will append each new experiment
-    exp_list = []
-
-    # Make list with all combinations of key values from our "batch dictionary"
-    combo_list = list(
-        itertools.product(
-            batch_dict["noise_scaling_factors"],
-            batch_dict["noise_scaling_method"],
-            batch_dict["extrapolation"],
-        )
-    )
-    # Iterate over the list
-    for k in range(len(combo_list)):
-        # Initialize single experiment dictionary with keys
-        exp = {
-            x: set()
-            for x in ["noise_scaling_factors", "noise_scaling_method", "extrapolation"]
-        }
-
-        # Map each key to its unique value from our list of combinations
-        exp["noise_scaling_factors"] = combo_list[k][0]
-        exp["noise_scaling_method"] = combo_list[k][1]
-        exp["extrapolation"] = combo_list[k][2]
-
-        # Pull out each experiment
-        exp_list.append(exp)
-
-    # Returns a list of experiments
-    return exp_list
-
-make_experiment_list(zne_batch_test)
-
-# Validate each experiment in our batch object:
-
-# Create function that takes in a batch dictionary and schema to validate against
-def batch_validate(batch_dict, schema):
-    # Initialize empty list for validation results
-    validation_results = []
-
-    # Create list of experiments
-    formatted_batch = make_experiment_list(batch_dict)
-
-    # Iterate over list of experiments and individually validate
-    for k in formatted_batch:
-        try:
-            jsonschema.validate(instance=k, schema=schema)
-            result = "validation passed"
-        except jsonschema.exceptions.ValidationError as e:
-            result = "validation failed"
-        except jsonschema.exceptions.SchemaError as e:
-            result = "schema validation failed"
-        # Pull out validation results
-        validation_results.append(result)
-
-    return validation_results
-
-batch_validate(zne_batch_test, zne_schema)
-
-noise_scaling_map = {
-    "global": fold_global,
-    "local_random": fold_gates_at_random,
-    "local_all": fold_all,
-    "layer": get_layer_folding,
-    "identity_scaling": insert_id_layers,
-}
-
-
-def extrapolation_map(single_exp):
-    ex_map = {
-        "linear": zne.inference.LinearFactory(
-            scale_factors=single_exp["noise_scaling_factors"]
-        ),
-        "richardson": zne.inference.RichardsonFactory(
-            scale_factors=single_exp["noise_scaling_factors"]
-        ),
-        "polynomial": zne.inference.PolyFactory(
-            scale_factors=single_exp["noise_scaling_factors"], order=2
-        ),
-        "exponential": zne.inference.ExpFactory(
-            scale_factors=single_exp["noise_scaling_factors"]
-        ),
-        "poly-exp": zne.inference.PolyExpFactory(
-            scale_factors=single_exp["noise_scaling_factors"], order=1
-        ),
-        "adaptive-exp": zne.inference.AdaExpFactory(
-            scale_factor=single_exp["noise_scaling_factors"][1], steps=4, asymptote=None
-        ),
-    }
-    return ex_map[single_exp["extrapolation"]]
-
-def make_executor(backend, verify, shots=4096):
-
-    def executor(circuit):
-        qc = circuit.copy()
-        qc.measure_all()
-        transpiled_qc = transpile(qc, backend=backend, optimization_level=0)
-
-        result = backend.run(transpiled_qc, shots=shots).result()
-        counts = result.get_counts(transpiled_qc)
-        
-        # Use verify function adapted to the experiment to calculate the expectation value 
-        expectation_value = verify(counts)
-
-        return expectation_value
-    
-    return executor
-
-def batch_execute(batch_dict, circuit, executor):
-    # Define list of experiments
-    formatted_batch = make_experiment_list(batch_dict)
-
-    # Initialize list to append expectation values into
-    exp_val_list = []
-
-    # Iterate over each experiment
-    for k in formatted_batch:
-        # tmp_executor = functools.partial(executor, noise_model=...)
-        exp_val = zne.execute_with_zne(
-            circuit=circuit,
-            executor=executor,
-            factory=extrapolation_map(k),
-            scale_noise=noise_scaling_map[k["noise_scaling_method"]],
-        )
-
-        # Pull out expectation values
-        exp_val_list.append(exp_val)
-
-    return exp_val_list
-
 
 # Create the circuit and verifying function of the experiment
 circ, verify_func, ideal_result= get_experiment(CIRCUIT)
+# circ, verify_func, ideal_result= get_experiment(CIRCUIT, num_qubits=5)
 
 # Create the backend given the noise model
 backend = get_noise_backend(NOISE_MODEL)
@@ -178,29 +28,29 @@ backend = get_noise_backend(NOISE_MODEL)
 exe=make_executor(backend, verify_func, shots=4096)
 
 
-ideal_ev = ideal_result
-noisy_ev=exe(circ)
-print("ideal EV:", ideal_ev)
-print(f"{NOISE_MODEL} EV:", noisy_ev)
+# Default parameters:  circuit, executor, ideal_result, zne_batch_test
+# zne_batch_test is optional, if it is not provided, there is a default search space
+
+# BRUTE FORCE SEARCH
+# brute_force_search(circ, exe, ideal_result)
+# brute_force_search(circ, exe, ideal_result, zne_batch_test)
 
 
-exp_results = batch_execute(zne_batch_test, circ, exe)
-for k in range(len(exp_results)):
-    print("Experiment", k, "Mitigated Expectation Value:", exp_results[k - 1])
+# LIGHT BRUTE FORCE SEARCH
+# max_iter=10 # optional
+# light_brute_force_search(circ , exe, ideal_result)
+# light_brute_force_search(circ , exe, ideal_result, zne_batch_test)
+# light_brute_force_search(circ , exe, ideal_result, zne_batch_test, max_iter)
 
 
-configs = make_experiment_list(zne_batch_test)
+# ADAPTATIVE ROUTINE (usually the user will not give the experiment parameters)
+# adaptive_search(circ , exe, ideal_result, zne_batch_test)
+adaptive_search(circ , exe, ideal_result)
+max_factors=10 # (optional) maximum number of scale factors
+tolerance=1e-4 # (optional) if the improvement is lower that the tolerance, stop expanding the list
+#adaptive_search(circ , exe, ideal_result, tolerance, max_factors)
 
-errors = np.abs(np.array(exp_results) - ideal_ev)
-best_idx = np.argmin(errors)
 
-
-print(f"BEST RESULT: Experiment #{best_idx + 1}")
-print(f"Ideal Value:     {ideal_ev}")
-print(f"Mitigated Value: {exp_results[best_idx]}")
-print(f"Absolute Error:  {errors[best_idx]}")
-print("WINNING CONFIGURATION:")
-print(json.dumps(configs[best_idx], indent=4))
 
 
 
